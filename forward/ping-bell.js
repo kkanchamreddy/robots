f869cd3627ffcd3be020cd90f77e7ec10ce86c69
kkanchamreddy <kiran.kanchamreddy@gmail.com>
2016-02-09 13:57:55 -0800

    ping-bell.js

---
+++ forward/ping-bell.js
@@ -0,0 +1,33 @@
+var five = require('johnny-five');
+var board = new five.Board();
+
+board.on('ready', function(){
+  var piezo = new five.Piezo(9);
+  // Plays a song
+  piezo.play({
+    // song is composed by an array of pairs of notes and beats
+    // The first argument is the note (null means "no note")
+    // The second argument is the length of time (beat) of the note (or non-note)
+    song: [
+      ["C4", 1 / 4],
+      ["D4", 1 / 4],
+      ["F4", 1 / 4],
+      ["D4", 1 / 4],
+      ["A4", 1 / 4],
+      [null, 1 / 4],
+      ["A4", 1],
+      ["G4", 1],
+      [null, 1 / 2],
+      ["C4", 1 / 4],
+      ["D4", 1 / 4],
+      ["F4", 1 / 4],
+      ["D4", 1 / 4],
+      ["G4", 1 / 4],
+      [null, 1 / 4],
+      ["G4", 1],
+      ["F4", 1],
+      [null, 1 / 2]
+    ],
+    tempo: 100
+  });
+});
